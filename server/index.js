4e2ad125a892f5b8fa3fa5e1d0bf88d47f5822ba
Eric Bartsch <ericbartsch@Erics-MacBook-Air.local>
2023-03-27 16:07:45 -0600

    remove get conditional

--- server/index.js
+++ server/index.js
@@ -23,8 +23,6 @@ app.use(cors());
 app.use(bodyParser.json());
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "/public/"));
-
-  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
 const port = process.env.PORT || 8000;
