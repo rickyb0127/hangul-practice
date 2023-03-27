b8a9b24d784ddf58f255f3b06aac07d40e27fd49
Eric Bartsch <ericbartsch@Erics-MacBook-Air.local>
2023-03-27 12:02:04 -0600

    serve from public folder

--- server/index.js
+++ server/index.js
@@ -19,6 +19,11 @@ const upload = multer({ storage }).single('testImage');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+if(process.env.NODE_ENV !== "production") {
+  app.use(express.static(__dirname + "/public/"));
+
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+}
 
 const port = process.env.PORT || 8000;
 
